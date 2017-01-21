233167558c35d24225bd7e18a5d34b77cd09690a
Spencer Pollock <srepollock@users.noreply.github.com>
2017-01-20 21:27:30 -0800

    Fixed @MattWongCode4U's Pull Request (#8)
    * :beer: Empty Project and Readme added/updated
    
    * Adding temp files so we have the folders
    
    * :beer: Added meta. Fixing up necessary files
    
    * Fixing merge

---
+++ Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    // Get the current player score
+    int getCurrentScore() {
+        return PlayerPrefs.GetInt("score");
+    }
+
+    // Add to the score a certain amount
+    void incrementScore(int increment) {
+        int score = getCurrentScore();
+        PlayerPrefs.SetInt("score", score + increment);
+    }
+}
