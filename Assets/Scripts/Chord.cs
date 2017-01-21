df89790ce6923b66a23f92727ce22c5039fd1d6a
Vincent Huang <poshenhuang123@hotmail.com>
2017-01-20 21:29:39 -0800

    some scripts

---
+++ Assets/Scripts/Chord.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chord : MonoBehaviour {
+	public Notes[] notes;
+	private float speed;
+
+	// Use this for initialization
+	void Start () {
+		notes = new Notes[3];
+		//Initially set as empty
+		notes[0] = Notes.empty;
+		notes[1] = Notes.empty;
+		notes[2] = Notes.empty;
+	}
+
+	//Use this function when you spawn to set notes
+	public void Initialize (Notes first, Notes second, Notes third){
+		notes[0] = first;
+		notes[1] = second;
+		notes[2] = third;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+	private Chord target;
+	public float bulletSpeed;
+	public GameObject bulletSpawn;
+
+	// Use this for initialization
+	void Start () {
+		target = new Chord ();
+	}
+
+	void setTarget (Notes first, Notes second, Notes third)
+	{
+		target.Initialize (first, second, third);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void shoot(int size) {
+		Chord bullet = new Chord ();
+		for (int i = 0; i < 3; i++){
+			if(i > size)
+				bullet.notes[i] = Notes.empty;
+			else
+				bullet.notes[i] = (Notes)Random.Range(0, 4);
+		}
+
+	}
+}
