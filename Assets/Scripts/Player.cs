d41a838a654e50e33d36e61e469a49af450fd6b1
Spencer Pollock <srepollock@users.noreply.github.com>
2017-01-21 17:19:09 -0800

    Adding UI files and collisions. Working and looking really good (#22)
    * :beer: Empty Project and Readme added/updated
    
    * Adding temp files so we have the folders
    
    * :beer: Added meta. Fixing up necessary files
    
    * Fixing merge
    
    * :sleepy: Added models. Base shooting works. Cleaned up some files and added comments. Going to have to do a nice clean soon. Next is going to build a working alpha version of the game
    
    * :sleepy: Added particle effects on the music note
    
    * :relaxed: Added UI for the notes to let the player know which buttons they are and need to shoot back
    
    * :relieve: Models fixed for rotation and origin points. Notes UI updated. Multinote UI working as well. Good to start merging with develop
    
    * :relieve: Enemy text UI added. Working on collision of notes, players and enemies. Then we will have a working alpha
    
    * :sunglasses: Collisions finished. User input finished. Ready to merge with team and get final updates before alpha

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,52 +1,91 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour {
+	/// <summary>
+    /// Lanes for the player to move in.
+    /// </summary>
 	public GameObject[] lanes;
+	/// <summary>
+    /// Chord spawn for the players chords.
+    /// </summary>
 	public GameObject chordSpawn, chordPrefab;
-	private int location =  0, setNote = 0;
+	/// <summary>
+    /// User input for chords to be displayed.
+    /// </summary>
+	public Text userInputText;
+	/// <summary>
+    /// Location of the user, ie lane they are in.
+    /// </summary>
+	private int location = 0;
+	/// <summary>
+    /// Number of notes allowed.
+    /// </summary>
+	private int setNote = 0;
+	/// <summary>
+    /// Input for notes from user.
+    /// </summary>
 	private Notes[] input = new Notes[3];
-
+	/// <summary>
+    /// Delay for the fire rate of the player.
+    /// </summary>
     private float fireDelay = 1f;
+	/// <summary>
+    /// Fired boolean for a current shot.
+    /// </summary>
     private bool fired = false;
 
+	void Awake()
+	{
+		clearArray(input);
+	}
+
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if (Input.GetKeyDown ("up")) {
+	void Update () 
+	{
+		if (Input.GetKeyDown ("up")) 
+		{
 			moveUp ();
-		} else if(Input.GetKeyDown("down")){
+		} else if(Input.GetKeyDown("down"))
+		{
 			moveDown ();
 		}
-
-		if (setNote <= 2) {
-			if (Input.GetKeyDown ("a")) {
+		if (setNote <= 2) 
+		{
+			if (Input.GetKeyDown ("a")) 
+			{
 				input [setNote] = Notes.A;
 				setNote++;
 			}
-			if (Input.GetKeyDown ("s")) {
+			if (Input.GetKeyDown ("s")) 
+			{
 				input [setNote] = Notes.S;
 				setNote++;
 			}
-			if (Input.GetKeyDown ("d")) {
+			if (Input.GetKeyDown ("d")) 
+			{
 				input [setNote] = Notes.D;
 				setNote++;
 			}
-			if (Input.GetKeyDown ("f")) {
+			if (Input.GetKeyDown ("f")) 
+			{
 				input [setNote] = Notes.F;
 				setNote++;
 			}
-			if (Input.GetKeyDown ("g")) {
+			if (Input.GetKeyDown ("g")) 
+			{
 				input [setNote] = Notes.G;
 				setNote++;
 			}
 		}
-
 		if (Input.GetKeyDown ("space")) {
             if (!fired)
             {
@@ -54,19 +93,15 @@ public class Player : MonoBehaviour {
                 {
                     input[setNote++] = Notes.Empty;
                 }
-
                 if (input[0] != Notes.Empty)
                 {
                     shoot();
                     fired = true;
                 }
-
-                Debug.Log("Chord Fired: " + input[0] + " " + input[1] + " " + input[2]);
                 setNote = 0;
-                input = new Notes[3];
-            }
+                input = new Notes[3];clearArray(input);
+			}
 		}
-
         if (fired)
         {
             fireDelay -= Time.deltaTime;
@@ -76,26 +111,64 @@ public class Player : MonoBehaviour {
                 fireDelay = 1f;
             }
         }
+		setTextBar(input, setNote);
 	}
-
-	//Move player up
+	/// <summary>
+    /// Move the player up.
+    /// </summary>
 	void moveUp(){
         //fired = false;
         if (location > 0)
 			transform.position = lanes [--location].transform.position;
 	}
-
-	//Move player down
+	/// <summary>
+    /// Moves the player down.
+    /// </summary>
 	void moveDown(){
         //fired = false;
         if (location < lanes.Length - 1)
 			transform.position = lanes [++location].transform.position;
 	}
-
-	//Shoot played chord
+	/// <summary>
+    /// Shoots the player cord.
+    /// </summary>
 	void shoot(){
 		GameObject bullet = (GameObject)Instantiate(chordPrefab, chordSpawn.transform.position, new Quaternion());
 		bullet.GetComponent<Chord>().Initialize (input, Direction.Right);
 		bullet.GetComponent<Chord> ().increaseSpeed ();
+	}
+	void clearArray(Notes[] n)
+	{
+		for (int i = 0; i < 3; i++) 
+		{
+			n[i] = Notes.Empty;
+		}
+	}
+	/// <summary>
+    /// Sets the text bar with the notes of the object
+    /// </summary>
+    /// <param name="_n">Notes of the player</param>
+	private void setTextBar(Notes[] _n, int notes)
+	{
+		string o = "";
+		if(notes != 0) 
+		{
+			for (int i = 0; i < _n.Length; i++)
+			{
+				o += _n[i].ToString() + " ";
+			}
+		}
+		userInputText.text = o;
+	}
+	/// <summary>
+	/// Collision handling for the Player and notes.
+    /// </summary>
+    /// <param name="col">Collider</param>
+	void OnTriggerEnter(Collider col)
+	{
+		if(col.gameObject.tag == "Note")
+		{
+			Destroy (gameObject);
+		}	
 	}
 }
